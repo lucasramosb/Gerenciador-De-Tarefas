3d5c708351bb17704f591dc08ccc7604b6f0293c
Lucas <lucasramos1123.lr29@gmail.com>
2022-11-17 20:05:52 -0300

    fix: change timeout alert

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import AlertTemplate from "react-alert-template-basic";
 const options = {
   // you can also just use 'bottom center'
   position: positions.BOTTOM_CENTER,
-  timeout: 2500,
+  timeout: 3000,
   offset: '30px',
   // you can also just use 'scale'
   transition: transitions.SCALE
