ffb687d88853482cbb55adf01ad2b52992ae939d
Lucas <lucasramos1123.lr29@gmail.com>
2023-05-11 18:28:38 -0300

    feat: add function to change a task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -22,13 +22,27 @@ const TaskItem = ({task, fetchTasks}) => {
         }
     }
 
+    const handleTaskCompletionChange = async (e) =>{
+        try {
+            await axios.patch(`https://fsc-task-manager-backend.herokuapp.com/tasks/${task._id}`, {
+                isCompleted: e.target.checked
+            });
+
+            await fetchTasks();
+
+            alert.success("A tarefa foi modificada com sucesso!")
+        } catch (error) {
+            alert.error("Algo deu errado!")
+        }
+    }
+
     return (
         <div className="task-item-container">
             <div className="task-description">
                 {/* se a tarefa estiver completa(task.isCompleted) usar a classe... */}
                 <label className={task.isCompleted ? 'checkbox-container-completed' : 'checkbox-container'}> 
                     {task.description}
-                    <input type="checkbox" defaultChecked={task.isCompleted} />
+                    <input type="checkbox" defaultChecked={task.isCompleted} onChange={(e) => handleTaskCompletionChange(e)} />
                     <span className={task.isCompleted ? 'checkmark completed' : 'checkmark'}></span>
                 </label>
             </div>
