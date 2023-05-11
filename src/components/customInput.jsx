9177f7855c5c3414935e0010499e5f3556becc27
Lucas <lucasramos1123.lr29@gmail.com>
2023-05-11 18:46:23 -0300

    feat: add enterOnPress prop

--- src/components/customInput.jsx
+++ src/components/customInput.jsx
@@ -1,11 +1,23 @@
 import './CustomInput.scss'
 
 //Component do input de adicionar tarefa
-const CustomInput = ({label, value, onChange}) => {
+const CustomInput = ({label, value, onChange, onEnterPress}) => {
+
+    const handleKeyDonw = (e) => {
+        if(e.key === "Enter"){
+            onEnterPress();
+        }
+    }
+
     return (
         //div com classe custon-input-container
         <div className="custom-input-container">    
-            <input type="text" className="custom-input" value={value} onChange={(e) => onChange(e)} />
+            <input
+             type="text" 
+             className="custom-input" 
+             value={value} 
+             onChange={(e) => onChange(e)} 
+             onKeyDown={(e) => handleKeyDonw(e)}/>
 
             { label ? (
                 <label className={`${value ? "shrink" : ""} custom-input-label`}>
