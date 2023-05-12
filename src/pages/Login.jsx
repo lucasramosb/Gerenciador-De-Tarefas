04f959f0c87bbb042c21e28b3788df6b487c4f5c
Lucas <lucasramos1123.lr29@gmail.com>
2023-05-12 19:36:50 -0300

    feat: add navigation to home page in Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,21 @@ import './Login.scss'
 import logo from '../assets/images/logo.png';
 
 import CustomButton from '../components/CustomButton';
-
+import { useNavigate } from 'react-router-dom';
 
 const Login = () =>{
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate('/')
+    }
+
     return (
         <div className="login-container">
             <img src={logo} alt="logo" />
 
             <div className="button-container">
-                <CustomButton> Entrar </CustomButton>
+                <CustomButton onClick={handleSignInClick}> Entrar </CustomButton>
             </div>
         </div>
     )
